e233af9aec3174a448f0b34f5e09d61aeef174ba
aparna_chinnaiah <aparna.chinnaiah@hipbar.com>
2018-11-21 17:56:54 +0530

    Modified server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express();
 
 app.get('*.js', function (req, res, next) {
-  //const runtimeUrlRegex = /runtime.*.js/
+  const runtimeUrlRegex = /runtime.*.js/
   if(!runtimeUrlRegex.test(req.url)) {
     req.url = req.url + '.gz';
     res.set('Content-Encoding', 'gzip');
